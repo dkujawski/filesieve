3e03d51838c76a0fb052afd3610c653df2e09ff2
Dave Kujawski <dave.kujawski@gmail.com>
2011-07-05 20:46:37 +1200

    adding new tests for clean_up and get_hash_key

--- test/test_sieve.py
+++ test/test_sieve.py
@@ -3,20 +3,20 @@ import os
 import shutil
 
 from nose.tools import assert_equal
+from nose.tools import assert_false
+from nose.tools import assert_true
 
 from filesieve import sieve
 
 class TestSieve(object):
-    def __init__(self):
-        self.test_dir = os.path.abspath(os.path.dirname(__file__))
-        return
-    def setup(self):        
+    def setup(self):
+        test_dir = os.path.abspath(os.path.dirname(__file__))        
         stamp = datetime.datetime.now().strftime('%Y%m%d%H%M%s')
-        self.base = os.path.join(self.test_dir, stamp)
+        self.base = os.path.join(test_dir, stamp)
         self.src = os.path.join(self.base, 'src')
         self.dup = os.path.join(self.base, 'dup')
         os.makedirs(self.dup)
-        data = os.path.join(self.test_dir, 'data')
+        data = os.path.join(test_dir, 'data')
         shutil.copytree(data, self.src)
         return    
     def teardown(self):
@@ -36,15 +36,26 @@ class TestSieve(object):
         assert_equal(b, expected)
         assert_equal(d, 2)
         return
+    def test_clean_dup(self):
+        dup_file = os.path.join(self.src, 'small_copy.log')
+        sieve.clean_dup(dup_file, self.dup)
+        expected = os.path.join(self.dup, dup_file.lstrip('/'))
+        assert_true(os.path.exists(expected))
+        assert_false(os.path.exists(dup_file))
+        return
     
 class TestSieveStaticFuncs(object):
     def setup(self):
+        test_dir = os.path.abspath(os.path.dirname(__file__))
+        self.data_dir = os.path.join(test_dir, 'data')
         return
     def teardown(self):
         return    
     def test_get_hash_key(self):
-        return
-    def test_clean_dup(self):
+        expected = "f6325473c8d80f37cf5711c1e97e92f0"
+        data = os.path.join(self.data_dir, 'small_orig.log')
+        found = sieve.get_hash_key(data)
+        assert_equal(expected, found)
         return
     
 if __name__ == '__main__':
@@ -54,5 +65,14 @@ if __name__ == '__main__':
     ts.setup()
     ts.test_walk()
     ts.teardown()
+    
+    ts.setup()
+    ts.test_clean_dup()
+    ts.teardown()
+
+    tssf = TestSieveStaticFuncs()
+    tssf.setup()
+    tssf.test_get_hash_key()
+    tssf.teardown()
     
     print '\ndone!'
